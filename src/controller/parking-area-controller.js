012bda8fc026d7ac5a33499728210b86b9aa43be
ArifAldini30 <aldiniii30@student.uns.ac.id>
2024-06-21 12:57:52 +0700

    aldini

---
+++ src/controller/parking-area-controller.js
@@ -0,0 +1,76 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getAllParkingAreas = async (req, res) => {
+  const parkingAreas = await prisma.parkingArea.findMany({
+    orderBy: { parking_name: "asc" },
+    include: { parkingSpaces: true },
+  });
+
+  res.json(parkingAreas);
+};
+
+const getAllParkingSpaces = async (req, res) => {
+  const parkingSpaces = await prisma.parkingSpace.findMany({
+    orderBy: { space_name: "asc" },
+  });
+
+  res.json(parkingSpaces);
+};
+
+const updateParkingAreas = async (req, res) => {
+  const parkingAreas = req.body;
+
+  await prisma.parkingArea.deleteMany();
+
+  for (const data of parkingAreas) {
+    await prisma.parkingArea.create({
+      data: {
+        id: data.area_id,
+        parking_name: data.parking_name,
+      },
+    });
+
+    for (const item of data.space_names) {
+      await prisma.parkingSpace.create({
+        data: {
+          parkingAreaId: data.area_id,
+          space_name: item,
+        },
+      });
+    }
+  }
+
+  res.status(201).json(parkingAreas);
+};
+
+const updateParkingSpaces = async (req, res) => {
+  const parkingSpaces = req.body;
+
+  for (const data of parkingSpaces) {
+    await prisma.parkingSpace.update({
+      where: { space_name: data.space_name },
+      data: {
+        status: Boolean(data.status),
+      },
+    });
+  }
+
+  res.status(200).json(parkingSpaces);
+};
+
+const deleteAllParkingAreas = async (req, res) => {
+  await prisma.parkingArea
+    .deleteMany()
+    .then(() => res.status(200).json([]))
+    .catch((err) => res.status(500).json(err));
+};
+
+export default {
+  getAllParkingAreas,
+  getAllParkingSpaces,
+  updateParkingAreas,
+  updateParkingSpaces,
+  deleteAllParkingAreas,
+};
--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,14 +1,10 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import parkingController from "../controller/parking-controller.js";
-import transactionController from "../controller/transaction-controller.js";
-import {
-  getAllAreas,
-  getAllMainAreas,
-  createMainAreas,
-  updateAllAreas,
-} from "../controller/area-controller.js";
 import gatesController from "../controller/gates-controller.js";
+// =========== ALDINI =============
+import parkingAreaController from "../controller/parking-area-controller.js";
+// =========== ALDINI =============
 
 const publicRouter = new express.Router();
 
@@ -16,28 +12,39 @@ const publicRouter = new express.Router();
 publicRouter.post("/api/users", userController.register);
 publicRouter.post("/api/user/login", userController.login);
 
-// TRANSACTION
-publicRouter.get("/api/transactions", transactionController.getTransaction);
-publicRouter.get("/api/transaction/:transactionId", transactionController.getTransactionById);
-publicRouter.patch("/api/transaction/:transactionId", transactionController.updateTransactionStatus);
-// HANDLE NOTIFICATION FROM MIDTRANS
-publicRouter.post("/api/transaction/notification", transactionController.trxNotif);
-
-
 // PARKING IN API
 publicRouter.post("/api/parkings/in", parkingController.parkingIn);
 publicRouter.post("/api/parkings/out", parkingController.parkingOut);
 
-// PUNYA ALDINI
-publicRouter.get("/api/mainareas", getAllMainAreas);
-publicRouter.post("/api/mainareas", createMainAreas);
-
-publicRouter.get("/api/areas", getAllAreas);
-publicRouter.post("/api/areas", updateAllAreas);
+// =========== ALDINI =============
+publicRouter.get(
+  "/api/parking_areas",
+  parkingAreaController.getAllParkingAreas
+);
+publicRouter.post(
+  "/api/parking_areas",
+  parkingAreaController.updateParkingAreas
+);
+publicRouter.post(
+  "/api/parking_areas/delete",
+  parkingAreaController.deleteAllParkingAreas
+);
+publicRouter.get(
+  "/api/parking_spaces",
+  parkingAreaController.getAllParkingSpaces
+);
+publicRouter.post(
+  "/api/parking_spaces",
+  parkingAreaController.updateParkingSpaces
+);
+// =========== ALDINI =============
 
 publicRouter.get("/api/gates", gatesController.getStatusGate);
-publicRouter.get("/api/opengates", gatesController.getStatusOpenGate);
-publicRouter.get("/api/closegates", gatesController.getStatusCloseGate);
+publicRouter.get("/api/opengates/:gatesId", gatesController.getStatusOpenGate);
+publicRouter.get(
+  "/api/closegates/:gatesId",
+  gatesController.getStatusCloseGate
+);
 publicRouter.post("/api/gates/in", gatesController.gateIn);
 publicRouter.post("/api/gates/createGate", gatesController.createGate);
 publicRouter.patch("/api/gates/update/:gatesId", gatesController.updateGate);
