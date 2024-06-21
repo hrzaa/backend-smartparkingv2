b81e3d51caa371b1ba1813bde3cede1a43b57ad1
hrzaa <harezayoan@gmail.com>
2024-06-21 14:45:18 +0700

    update router

--- src/routes/public-api.js
+++ src/routes/public-api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userController from "../controller/user-controller.js";
 import parkingController from "../controller/parking-controller.js";
+import transactionController from "../controller/transaction-controller.js";
 import gatesController from "../controller/gates-controller.js";
 // =========== ALDINI =============
 import parkingAreaController from "../controller/parking-area-controller.js";
@@ -8,13 +9,28 @@ import parkingAreaController from "../controller/parking-area-controller.js";
 
 const publicRouter = new express.Router();
 
-// LOGIN RESGISTER API
+// LOGIN RESGISTER
 publicRouter.post("/api/users", userController.register);
 publicRouter.post("/api/user/login", userController.login);
 
-// PARKING IN API
+// TRANSACTION
+publicRouter.get("/api/transactions", transactionController.getTransaction);
+publicRouter.get("/api/transaction/:transactionId", transactionController.getTransactionById);
+publicRouter.patch("/api/transaction/:transactionId", transactionController.updateTransactionStatus);
+// HANDLE NOTIFICATION FROM MIDTRANS
+publicRouter.post("/api/transaction/notification", transactionController.trxNotif);
+
+// PARKING
 publicRouter.post("/api/parkings/in", parkingController.parkingIn);
 publicRouter.post("/api/parkings/out", parkingController.parkingOut);
+
+// GATES 
+publicRouter.get("/api/gates", gatesController.getStatusGate);
+publicRouter.get("/api/opengates", gatesController.getStatusOpenGate);
+publicRouter.get("/api/closegates", gatesController.getStatusCloseGate);
+publicRouter.post("/api/gates/in", gatesController.gateIn);
+publicRouter.post("/api/gates/createGate", gatesController.createGate);
+publicRouter.patch("/api/gates/update/:gatesId", gatesController.updateGate);
 
 // =========== ALDINI =============
 publicRouter.get(
@@ -39,17 +55,5 @@ publicRouter.post(
 );
 // =========== ALDINI =============
 
-publicRouter.get("/api/gates", gatesController.getStatusGate);
-publicRouter.get("/api/opengates/:gatesId", gatesController.getStatusOpenGate);
-publicRouter.get(
-  "/api/closegates/:gatesId",
-  gatesController.getStatusCloseGate
-);
-publicRouter.post("/api/gates/in", gatesController.gateIn);
-publicRouter.post("/api/gates/createGate", gatesController.createGate);
-publicRouter.patch("/api/gates/update/:gatesId", gatesController.updateGate);
-// publicRouter.post("/api/gates/closeGate", gatesController.closeGate);
-
-// publicRouter.get("/api/parkings/counts", parkingController.countAllParking);
 
 export { publicRouter };
