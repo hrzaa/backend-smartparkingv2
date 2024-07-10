5ae325ef7dc955568c6d070b882b46b1f21482d8
hrzaa <harezayoan@gmail.com>
2024-07-10 16:29:36 +0700

    midtrans production

--- src/service/midtransService.js
+++ src/service/midtransService.js
@@ -1,7 +1,7 @@
 import midtransClient from "midtrans-client";
 
 const snap = new midtransClient.Snap({
-  isProduction: false, // Atur ke true untuk produksi
+  isProduction: true, // Atur ke true untuk produksi
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
